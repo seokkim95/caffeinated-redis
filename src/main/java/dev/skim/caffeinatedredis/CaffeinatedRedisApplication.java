eccaf45646832701aaa12c21a561bd2192c0e8ae
Seok Hoon Kim <seokkim917@gmail.com>
2026-01-17 00:59:54 -0500

    init

---
+++ src/main/java/dev/skim/caffeinatedredis/CaffeinatedRedisApplication.java
@@ -0,0 +1,13 @@
+package dev.skim.caffeinatedredis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaffeinatedRedisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaffeinatedRedisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/skim/caffeinatedredis/CaffeinatedRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.skim.caffeinatedredis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaffeinatedRedisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
